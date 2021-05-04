ad054072e067dde26f05e10ca6c54a09ea64c6c8
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2021-05-04 11:32:28 +0800

    Add vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,74 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-trunk',
+    'version' => 'dev-trunk',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'cb909634eb88347c277c3f112359e5c07d42ace9',
+    'name' => 'woocommerce/wc-smooth-generator',
+  ),
+  'versions' => 
+  array (
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v1.10.0',
+      'version' => '1.10.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1a0357fccad9d1cc1ea0c9a05b8847fbccccb78d',
+    ),
+    'fzaninotto/faker' => 
+    array (
+      'pretty_version' => 'v1.9.2',
+      'version' => '1.9.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '848d8125239d7dbf8ab25cb7f054f1a630e68c2e',
+    ),
+    'jdenticon/jdenticon' => 
+    array (
+      'pretty_version' => '0.10.0',
+      'version' => '0.10.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '31e60b2ae6b3c3ca6be0d19d14a7670f04b173ce',
+    ),
+    'mbezhanov/faker-provider-collection' => 
+    array (
+      'pretty_version' => '1.2.1',
+      'version' => '1.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '076c00f0d438f12ec7da0fdaadbfb7940913763e',
+    ),
+    'roundcube/plugin-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'shama/baton' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'woocommerce/wc-smooth-generator' => 
+    array (
+      'pretty_version' => 'dev-trunk',
+      'version' => 'dev-trunk',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cb909634eb88347c277c3f112359e5c07d42ace9',
+    ),
+  ),
+);
